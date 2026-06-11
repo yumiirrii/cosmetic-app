ea551e22af4b2649ee948cbf4064f3bdf5b7a52c
irisawa_yumiko <162bis@gmail.com>
2026-06-11 17:31:16 +0900

    update validation

--- app/schemas/product.ts
+++ app/schemas/product.ts
@@ -1,28 +1,28 @@
 import z from "zod";
 
+const REQUIRED_MSG = "Required";
+
 export const categorySchema = z.enum(["cleanser", "toner", "serum", "cream"]);
 export type Category = z.infer<typeof categorySchema>;
 
 export const productSchema = z.object({
     category: z
         .union([categorySchema, z.literal("")])
-        .refine((val) => val !== "", { message: "Category is mandatory." }),
+        .refine((val) => val !== "", { message: REQUIRED_MSG }),
     isNew: z.boolean(),
-    name: z.string().min(1, "Product Name is mandatory."),
+    name: z.string().min(1, REQUIRED_MSG).max(100, "Maximum 100 characters"),
     price: z
-        .union([
-            z.coerce.number().min(1, "Price should be greater than 1."),
-            z.literal(""),
-        ])
-        .refine((val) => val !== "", { message: "Price is mandatory." }),
-    description: z
-        .string()
-        .max(250, "Description should be less than 250 characters."),
-    imageUrl: z.string().min(1, "Image Url is mandatory."),
+        .union([z.coerce.number(), z.literal("")])
+        .refine((val) => val !== "", { message: REQUIRED_MSG })
+        .refine((val) => Number(val) >= 1, {
+            message: "Minimum 1",
+        }),
+    description: z.string().max(250, "Maximum 250 characters"),
+    imageUrl: z.string().min(1, REQUIRED_MSG),
 });
 
 export const updateProductSchema = productSchema.extend({
-    id: z.string().min(1, "ID is mandatory."),
+    id: z.string().min(1, REQUIRED_MSG),
 });
 
 export type ProductInput = z.infer<typeof productSchema>;
